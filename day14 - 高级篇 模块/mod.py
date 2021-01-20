bff680b227760deb5d6702fb2b4c747a515d4bce
阳之幸福 <1689484241@qq.com>
2021-01-20 08:25:57 +0800

    2021年 1月20日 星期三 08时25分24秒 CST day14 - 高级篇 - 模块 --- 模块的特殊属性__name__

--- day14 - 高级篇 模块/mod.py
+++ day14 - 高级篇 模块/mod.py
@@ -1,8 +1,26 @@
 v = 56
 
+
 def f():
     print('f被调用')
 
 
 class SomeClass:
     pass
+
+
+print('模块的特殊属性__name__的值为:{}'.format(__name__))
+
+"""
+以下是测试代码
+"""
+
+
+def add_num(num1, num2):
+    return num1 + num2
+
+
+# 根据__name__的值判断是否执行模块中的测试代码。
+if __name__ == '__main__':
+    print('1 + 2 =', add_num(1, 2))
+    print('3 + 5 = ', add_num(3, 5))
