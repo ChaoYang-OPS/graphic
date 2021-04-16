cf9ea963a0e050e8c7dcdf255d94ce78a63ade37
阳之幸福 <1689484241@qq.com>
2021-04-16 13:16:30 +0800

    Revert "Thu Apr 15 20:47:24 CST 2021 update groovy file"
    This reverts commit 8d372a64be04c78e48d3187345b1ab550643affe.

--- day21 - Code/jenkins-learning/共享库/src/com/opsk8s/tools.groovy
+++
@@ -1,8 +0,0 @@
-//格式化输出
-def print_msg(value,color){
-    colors = ['red'   : "\033[40;31m #####################${${value}#####################${ \033[0m",
-              'green' : "\033[40;32m #####################${${value}#####################${ \033[0m" ]
-    ansiColor('xterm') {
-        println(colors[color])
-    }
-}
