1e35139bb7c5ffec3058a0eab32f9517b9e78126
阳之幸福 <1689484241@qq.com>
2021-04-21 22:40:30 +0800

    add tools file exec_shell_command_return_status

--- day21 - Code/devops/tools.groovy
+++ day21 - Code/devops/tools.groovy
@@ -10,3 +10,19 @@ def PrintMes(value,color){
         println(colors[color])
     }
 }
+
+
+def exec_shell_command_return_status(shell_command){
+    def shell_command_return_status = "-1"
+
+    def shell_command_return_status = sh encoding: "UTF-8", returnStatus: true, returnStdout: true, script: "${shell_command}"
+
+    if (shell_command.length() != 0 && shell_command_return_status == 0){
+        def shell_command_return_response = sh returnStdout: true, script: "${shell_command}"
+        // println("${shell_command_return_response}")
+        // println("[success] execution [${shell_command}] with return : [${shell_command_return_status}]")
+    }else{
+        println("[error] execution [${shell_command}] with return : [${command_return_status}]")
+    }
+    return shell_command_return_status
+}
