f7ccc098f232394f3285ff72dafd7e58f09803c2
阳之幸福 <1689484241@qq.com>
2021-04-22 08:22:47 +0800

    Global variables cannot be shared in more than one process and Global variables can be shared across all threads of the process

--- day20 - 高级进阶篇/22 全局变量在进程的所有线程中可以共享.py
+++ day20 - 高级进阶篇/22 全局变量在进程的所有线程中可以共享.py
@@ -1,2 +1,20 @@
 """全局变量在进程的所有线程中可以共享"""
 
+from threading import Thread
+
+"""
+    进程内的所有线程共享内存空间，所以，全局变量在进程的所有线程中可以共享。
+"""
+
+num = 18
+
+def do_sth():
+    global num
+    num += 1
+
+if __name__ == '__main__':
+
+    t = Thread(target=do_sth)
+    t.start()
+    t.join()
+    print(num)  # 19
