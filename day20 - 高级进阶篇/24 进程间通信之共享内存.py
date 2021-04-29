68b9269d636f72f7dff2e9c0931ddc29b4511fa2
阳之幸福 <1689484241@qq.com>
2021-04-29 21:24:46 +0800

    The process and thread deadlock

--- day20 - 高级进阶篇/24 进程间通信之共享内存.py
+++ day20 - 高级进阶篇/24 进程间通信之共享内存.py
@@ -1,1 +1,42 @@
 """进程间通信之共享内存"""
+
+"""
+    如果想要实现进程之间的通信，共享内存是常见的实现方式之一。它允许多个进程直接访问同一块内存。
+    共享内存中对象的类型必须是ctypes的。ctypes是与C语言兼容的数据类型。
+    
+    为了在共享内存中创建ctypes类型的对象，标准库模块multiprocessing中提供如下两个函数:
+    
+（1） Value(typecode_or_type, *args, **kwargs):
+    返回值表示一个数值。
+    参数: typecode_or_type用于指定数值的类型码或ctypes类型。
+（2） Array(typecode_or_type, size_or_initializer, lock=True)
+    返回值表示一个数据。
+    参数: typecode_or_type用于指定数组中元素的类型码或ctypes类型。
+    参数: size_or_initializer用于指定数组的长度或Python中的序列。
+    
+"""
+
+from multiprocessing import Process, Value, Array
+import ctypes
+
+# 在共享内存中创建一个表示数值的ctypes对象
+num = Value('d', 2.3)
+# num = Value(ctypes.c_float, 2.3)
+
+# 在共享内存中创建一个表示数组的ctypes对象
+arr = Array('i', range(1, 5))
+
+
+# arr = Array(ctypes.c_int, range(1, 5))
+
+def do_sth():
+    num.value = 1.8
+    for i in range(len(arr)):
+        arr[i] = -arr[i]
+
+
+p = Process(target=do_sth)
+p.start()
+p.join()
+print(num.value)
+print(arr[:])
---
+++ day20 - 高级进阶篇/28 多进程的死锁.py
@@ -0,0 +1,66 @@
+"""多进程的死锁"""
+
+"""
+    如果有多个共享数据，在多个进程操作这多个共享数据时，如果两个进程分别通过加锁占有一部分共享数据，
+并且同时等待对方释放锁，这样就会导致两个进程永远相互等待而产生死锁。
+    要避免程序中出现死锁的情况。在避免死锁的算法中最有代表性的算法是Dijkstra提出的银行家算法。
+"""
+
+from multiprocessing import Process, Lock, Value, current_process
+
+numa = Value('i', 0)
+numb = Value('i', 0)
+
+locka = Lock()
+lockb = Lock()
+
+
+def do_sth():
+    fun1()
+    fun2()
+
+
+def fun1():
+    global numa, numb
+
+    locka.acquire()
+    try:
+        print('%s---fun1()----locka' % current_process().pid)
+        numa.value += 1
+        lockb.acquire()
+        try:
+            print('%s---fun1()----lockb' % current_process().pid)
+            numb.value += 1
+        finally:
+            lockb.release()
+    finally:
+        locka.release()
+
+
+def fun2():
+    global numa, numb
+
+    locka.acquire()
+    try:
+        print('%s---fun2()----lockb' % current_process().pid)
+        numb.value += 1
+        locka.acquire()
+        try:
+            print('%s---fun2()----lockb' % current_process().pid)
+            numa.value += 1
+        finally:
+            locka.release()
+    finally:
+        lockb.release()
+
+
+p_list = []
+for i in range(100):
+    p = Process(target=do_sth)
+    p_list.append(p)
+    p.start()
+
+for item in p_list:
+    item.join()
+
+print('父进程结束')
