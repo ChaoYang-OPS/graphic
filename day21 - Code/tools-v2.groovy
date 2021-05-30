41b78dff6637704a1cff8a002b481ae3a3711254
阳之幸福 <1689484241@qq.com>
2021-05-31 07:49:39 +0800

    update tools-v2 add get_git_commits_id

--- day21 - Code/tools-v2.groovy
+++ day21 - Code/tools-v2.groovy
@@ -12,15 +12,19 @@ def print_core_msg(value,color){
 def get_scm_code(git_http_url, branche_name, git_http_credentials_id){
     print_core_msg("check SCM git_http_url=${git_http_url} ","green")
     print_core_msg("check SCM branche_name=${branche_name}","green")
-    checkout([$class: 'GitSCM', branches: [[name: "${branche_name}"]],
-    extensions: [], userRemoteConfigs: [[credentialsId: "${git_http_credentials_id}",
+    checkout([$class: 'GitSCM', branches: [[name: "${branche_name}"]], 
+    extensions: [], userRemoteConfigs: [[credentialsId: "${git_http_credentials_id}", 
     url: "${git_http_url}"]]])
+
+    // commits_id = get_git_commit_id()
+    get_git_commit_id()
+    print_core_msg("check SCM commits_id=${env.git_commits_id}","green")
 }
 
 
 def docker_login_to_nexus_register_server(nexus_server,nexus3_server_credentials_id){
 
-    withCredentials([usernamePassword(credentialsId: "${nexus3_server_credentials_id}",
+    withCredentials([usernamePassword(credentialsId: "${nexus3_server_credentials_id}", 
     passwordVariable: 'NEXUS_PASSWORD', usernameVariable: 'NEXUS_USER')]) {
     // some block
         // docker login 172.16.100.99:8089 -u admin -p admin
@@ -31,7 +35,7 @@ def docker_login_to_nexus_register_server(nexus_server,nexus3_server_credentials
 
 def docker_login_to_register_server(register_server,register_server_credentials_id){
 
-    withCredentials([usernamePassword(credentialsId: "${register_server_credentials_id}",
+    withCredentials([usernamePassword(credentialsId: "${register_server_credentials_id}", 
     passwordVariable: 'REGISTER_PASSWORD', usernameVariable: 'REGISTER_USER')]) {
         def login_command = "docker login ${register_server} -u ${REGISTER_USER} -p ${REGISTER_PASSWORD} "
         exec_shell_command_return_status(login_command)
@@ -83,7 +87,7 @@ def build_image(build_type,register_server,image_name, image_tag){
 
 def docker_logout_to_nexus_register_server(nexus_server){
 
-    withCredentials([usernamePassword(credentialsId: "${nexus3_server_credentials_id}",
+    withCredentials([usernamePassword(credentialsId: "${nexus3_server_credentials_id}", 
     passwordVariable: 'NEXUS_PASSWORD', usernameVariable: 'NEXUS_USER')]) {
         def logout_command = "docker logout ${nexus_server}"
         exec_shell_command_return_status(logout_command)
@@ -91,17 +95,17 @@ def docker_logout_to_nexus_register_server(nexus_server){
 }
 def input_request_approved(input_message, input_description, input_submitter, input_name){
     input message: "${input_message}", ok: 'Yes',
-    parameters: [choice(choices: ['Yes', 'No'],
-    description: "${input_description}", name: "${input_name}")],
-    submitter: "${input_submitter}",
+    parameters: [choice(choices: ['Yes', 'No'], 
+    description: "${input_description}", name: "${input_name}")], 
+    submitter: "${input_submitter}", 
     submitterParameter: "${input_name}"
     println(parameters)
 }
 def build_scm_code(build_type, build_command){
     def build_tools = ["maven": "/usr/local/maven-3.8.1/bin",
-                       "gradle": "/usr/local/gradle-6.8.3/bin",
-                       "go": "/usr/local/go/bin",
-                       "sonar": "/usr/local/sonar-scanner-4.6.0.2311/bin",
+                       "gradle": "/usr/local/gradle-6.8.3/bin", 
+                       "go": "/usr/local/go/bin", 
+                       "sonar": "/usr/local/sonar-scanner-4.6.0.2311/bin", 
                        "nodejs": "/usr/local/node-v14.16.1/bin"]
     print_core_msg("current build_type=${build_type}", "green")
     print_core_msg("current build_command=${build_command}", "green")
@@ -122,7 +126,7 @@ def build_scm_code(build_type, build_command){
             break
         default:
             print_msg("unknown build_type ==> [maven|gralde|nodejs|go]", "green")
-
+            
     }
 }
 
@@ -147,7 +151,7 @@ def exec_shell_command_return_status(shell_command){
              delete_command = "rm -f ${shell_command_return_status_error_msg} &>/dev/null"
              exec_shell_command_return_status(delete_command)
         }
-
+        
     }
     return shell_command_return_status
 }
@@ -189,6 +193,30 @@ def get_create_version_time_and_build_id() {
 
 
 
+// def set_output_font_color_to_red(message){
+//     ansiColor ('xterm'){
+//         sh "echo \"\\033[1;031m${message} \\033[0m\""
+//     }
+// }
+
+// def set_output_font_color_to_green(message){
+//     ansiColor ('xterm'){
+//         sh "echo \"\\033[1;032m${message} \\033[0m\""
+//     }
+// }
+
+// def set_output_font_color_to_blue(message){
+//     ansiColor ('xterm'){
+//         sh "echo \"\\033[1;034mm${message} \\033[0m\""
+//     }
+// }
+
+// def set_output_font_color_to_yellow(message){
+//     ansiColor ('xterm'){
+//         sh "echo \"\\033[1;033mm${message} \\033[0m\""
+//     }
+// }
+
 
 def print_msg(value,color){
     colors = ['red'   : "\033[1;031m ${value} \033",
@@ -198,4 +226,23 @@ def print_msg(value,color){
     ansiColor('xterm') {
         println(colors[color])
     }
+}
+
+def get_git_commit_id(){
+    def get_commits_id_command = "git rev-parse --short HEAD"
+    commits_id = exec_shell_command_return_response(get_commits_id_command)
+    env.git_commits_id = commits_id
+    return commits_id
+}
+
+def nexus_artifact_plugin_upload(artifact_id, file_name, file_type,group_id, nexus_repository, app_version){
+    def nexus3_server_credentials_id = "0b52bd27-d8dd-42de-b394-1cd6f384f932"
+    def nexus_server_url = "172.16.100.99:8081"
+    def nexus_server_protocol = "http"
+    def nexus_version = "nexus3"
+    nexusArtifactUploader artifacts: [[artifactId: "${artifact_id}", classifier: '', file: "${file_name}", type: "${file_type}"]], 
+    credentialsId: "${nexus3_server_credentials_id}", 
+    groupId: "${group_id}", nexusUrl: "${nexus_server_url}", 
+    nexusVersion: "${nexus_version}", protocol: "${nexus_server_protocol}", 
+    repository: "${nexus_repository}", version: "${app_version}"
 }
